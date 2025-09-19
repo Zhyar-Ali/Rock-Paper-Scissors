c296b589eded3955e2faa853ce9b42d4d0bbf6ed
Zhyar <zhiarali595@gmail.com>
2025-09-19 20:07:48 +0300

    Fix previous functions & add playRound function
    Fix computerChoice & humanChoice function and add
    playRound function

--- scripts.js
+++ scripts.js
@@ -4,19 +4,55 @@ let computerScore=0;
 
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*3)+1;
-    switch(choice){
-        case 1:
-            return "Rock";
-            break;
-        case 2:
-            return "Paper";
-            break;
-        case 3:
-            return "Scissors";
-            break;
-    }
+    return choice;
 }
 
 function getHumanChoice(){
-    return prompt();
+    let choice = prompt().toLowerCase();
+    switch (choice){
+        case "rock":
+            return 1;
+        case "paper":
+            return 2;
+        case "scissors":
+            return 3;
+    }
+}
+
+function playRound(humanChoice, computerChoice){
+    switch(humanChoice){
+        case 1:
+            if (computerChoice==1){
+                console.log("Draw");
+            }else if (computerChoice==2){
+                console.log("you lose");
+                computerScore++;
+            }else {
+                console.log("you win");
+                humanScore++;
+            }
+            break;
+        case 2:
+            if (computerChoice==1){
+                console.log("You win");
+                humanScore++;
+            }else if (computerChoice==2){
+                console.log("Draw");
+            }else {
+                console.log("you lose");
+                computerScore++;
+            }
+            break;
+        case 3:
+            if (computerChoice==1){
+                console.log("you lose");
+                computerScore++;
+            }else if (computerChoice==2){
+                console.log("you win");
+                humanScore++;
+            }else {
+                console.log("Draw");
+            }
+            break;
+    }
 }
