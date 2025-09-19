3ab23951d157de69787f3563e20bd03a5ad5e15f
Zhyar <zhiarali595@gmail.com>
2025-09-19 20:18:06 +0300

    add playGame function to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 let humanScore=0;
 let computerScore=0;
 
-
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*3)+1;
     return choice;
@@ -25,30 +24,30 @@ function playRound(humanChoice, computerChoice){
             if (computerChoice==1){
                 console.log("Draw");
             }else if (computerChoice==2){
-                console.log("you lose");
+                console.log("you lose. Paper beats rock!");
                 computerScore++;
             }else {
-                console.log("you win");
+                console.log("you win. Rock beats scissors!");
                 humanScore++;
             }
             break;
         case 2:
             if (computerChoice==1){
-                console.log("You win");
+                console.log("You win. Paper beats rock!");
                 humanScore++;
             }else if (computerChoice==2){
                 console.log("Draw");
             }else {
-                console.log("you lose");
+                console.log("you lose. Scissors beats paper!");
                 computerScore++;
             }
             break;
         case 3:
             if (computerChoice==1){
-                console.log("you lose");
+                console.log("you lose. Rock beats scissors!");
                 computerScore++;
             }else if (computerChoice==2){
-                console.log("you win");
+                console.log("you win. Scissors beats paper!");
                 humanScore++;
             }else {
                 console.log("Draw");
@@ -56,3 +55,14 @@ function playRound(humanChoice, computerChoice){
             break;
     }
 }
+
+function playGame(){
+
+    for (let i=1; i<=5; i++){
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+        playRound(humanChoice, computerChoice);
+        console.log(`Player's Score: ${humanScore}, Computer's Score: ${computerScore}`);
+    }
+}
+playGame();
